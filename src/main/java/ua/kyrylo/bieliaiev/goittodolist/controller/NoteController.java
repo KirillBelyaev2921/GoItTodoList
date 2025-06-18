78a37726c49c52aac21db734b551e72605007030
Kyrylo Bieliaiev <kirillbelyaev2921@gmail.com>
2025-06-18 20:51:02 +0300

    Complete module 18: Rest API

--- src/main/java/ua/kyrylo/bieliaiev/goittodolist/controller/NoteController.java
+++ src/main/java/ua/kyrylo/bieliaiev/goittodolist/controller/NoteController.java
@@ -3,47 +3,43 @@ package ua.kyrylo.bieliaiev.goittodolist.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ua.kyrylo.bieliaiev.goittodolist.model.Note;
 import ua.kyrylo.bieliaiev.goittodolist.service.NoteService;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@Controller
-@RequestMapping("/note")
+@RestController
+@RequestMapping("/notes")
 public class NoteController {
 
     private final NoteService noteService;
 
-    @GetMapping("/list")
-    public String list(Model model) {
-        List<Note> notes = noteService.listAll();
-        model.addAttribute("notes", notes);
-        return "note/list";
+    @GetMapping
+    public List<Note> findAll() {
+        return noteService.listAll();
     }
 
-    @PostMapping("/delete")
-    public String delete(@RequestParam(name = "id") Long id) {
+    @GetMapping("/{id}")
+    public Note findById(@PathVariable Long id) {
+        return noteService.getById(id);
+    }
+
+    @PostMapping
+    public Note add(@RequestBody Note note) {
+        return noteService.add(note);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
         noteService.deleteById(id);
-        return "redirect:/note/list";
     }
 
-    @GetMapping("/edit")
-    public String edit(@RequestParam(name = "id") Long id, Model model) {
-        Note note = noteService.getById(id);
-        model.addAttribute("note", note);
-        return "note/edit";
-    }
-
-    @PostMapping(path = "/edit")
-    public String edit(@ModelAttribute("note") Note note) {
+    @PatchMapping("/{id}")
+    public Note edit(@PathVariable Long id, @RequestBody Note note) {
+        note.setId(id);
         noteService.update(note);
-        return "redirect:/note/list";
+        return findById(id);
     }
 }
